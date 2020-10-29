2cee71c780ea4be9b1f3d0f57e43b239fc0d9452
bueongi <67132017+bueongi@users.noreply.github.com>
2020-10-29 16:40:52 +0900

    Add files via upload

--- js/pc.js
+++ js/pc.js
@@ -1,11 +1,19 @@
 $(function(){
+  // 스킵메뉴
   $('.skip_menu a').focus(function(){
     $('.skip_menu').addClass('active');
-  })
+  });
   $('.skip_menu a').blur(function(){
     $('.skip_menu').removeClass('active');
-  }) 
+  });
+  // 배너 닫기
   $('#banner01_close').on('click',function(){
     $('.banner01').slideUp(200);
-  })
+  });
+  // 박스오피스 슬라이더
+  var swiper01 = new Swiper('.swiper-container01',{
+    spaceBetween:32,
+    slidesPerView:'auto',
+  });
 })
+// 
